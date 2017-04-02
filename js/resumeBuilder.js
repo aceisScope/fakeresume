ce2c25f8325fc52ce3004af14c81552c7b4ee644
Binghui Liu <bertieliu52@gmail.com>
2017-04-02 14:17:47 +0300

    add pic, contacts and skills to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,43 @@
-var formattedName = HTMLheaderName.replace("%data%", "Binghui");
-var formattedRole = HTMLheaderRole.replace("%data%", "Company cat");
+var bio = {
+  "name": "Binghui",
+  "role": "Company cat",
+  "contacts": {
+    "email": "bertieliu52@gmail.com",
+    "github": "@aceisScope",
+    "location": "Espoo"
+  },
+  "welcomeMsg": "Welcome to the page of the cutest company cat",
+  "skills": [
+    "purr", "roll on the floor"
+  ],
+  "bioPic": "images/avatar.jpeg"
+}
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var replace_text = "%data%";
+
+var name = HTMLheaderName.replace(replace_text, "Binghui");
+var role = HTMLheaderRole.replace(replace_text, "Company cat");
+
+var pic = HTMLbioPic.replace(replace_text, bio["bioPic"]);
+var welcomeMsg = HTMLwelcomeMsg.replace(replace_text, bio["welcomeMsg"]);
+
+$("#header").prepend(welcomeMsg);
+$("#header").prepend(role);
+$("#header").prepend(name);
+$("#header").prepend(pic);
+
+var skillStart = HTMLskillsStart;
+$("#header").append(skillStart);
+
+for (let skill of bio["skills"]) {
+  var html_skill = HTMLskills.replace(replace_text, skill);
+  $("#skills").append(html_skill);
+}
+
+var email = HTMLemail.replace(replace_text, bio["contacts"]["email"]);
+var github = HTMLgithub.replace(replace_text, bio["contacts"]["github"]);
+var place = HTMLlocation.replace(replace_text, bio["contacts"]["location"]);
+
+$("#topContacts").append(email);
+$("#topContacts").append(github);
+$("#topContacts").append(place);
